da98aa400566faf016b96681fe7236ba3b4167ad
Nils <nils@dr-plano.com>
2023-05-06 07:25:56 +0200

    ✨ improve utils

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -42,20 +42,7 @@ germanJson.filter((item, index, self) => {
 export type Translation = ManischToGerman | GermanToManisch;
 
 export const manischToGerman = manischEntries as ManischToGerman[];
-
 export const germanToManisch = germanEntries as GermanToManisch[];
-
-console.log('manischToGerman', JSON.stringify(manischToGerman));
-console.log('germanToManisch', JSON.stringify(germanToManisch));
-
-// Overload signatures
-function greet(person: string): string;
-
-// Implementation signature
-function greet(person: unknown): string {
-  // ...
-  throw new Error('Unable to greet');
-}
 
 export const getTranslation = (id : number) : GermanToManisch => {
 	const translation = germanToManisch.find((item) => item.id === id);
@@ -65,17 +52,29 @@ export const getTranslation = (id : number) : GermanToManisch => {
 	return translation;
 }
 
-export function getTranslationEntry(input: { manisch: string }) : ManischToGerman;
-export function getTranslationEntry(input: { german: string }) : GermanToManisch;
+export function getTranslationEntry(input: { manisch: string | number }) : ManischToGerman;
+export function getTranslationEntry(input: { german: string | number }) : GermanToManisch;
 export function getTranslationEntry(input: {
-	german : string
+	german : string | number
 } | {
-	manisch : string
+	manisch : string | number
 }) : ManischToGerman | GermanToManisch {
 	if ('manisch' in input) {
-		return manischToGerman.find((item) => item.manisch === input.manisch);
+		return manischToGerman.find((item) => {
+			if (typeof input.manisch === 'number') {
+				return item.id === input.manisch;
+			} else {
+				return item.manisch === input.manisch;
+			}
+		});
 	} else if ('german' in input) {
-		return germanToManisch.find((item) => item.german === input.german);
+		return germanToManisch.find((item) => {
+			if (typeof input.german === 'number') {
+				return item.id === input.german;
+			} else {
+				return item.german === input.german;
+			}
+		});
 	} else {
 		throw new Error('Translation not found');
 	}
@@ -89,15 +88,20 @@ export const getTranslationWords = (input: { german: string } | { manisch: strin
 		const translationEntry = getTranslationEntry(input);
 		const translationWordIds = translationEntry?.germanIds ?? [];
 		const words = germanToManisch
-			.filter((item) => translationWordIds.includes(item.id))
+			.filter((item) => {
+				return translationWordIds.includes(item.id)
+			})
 			.map(item => item.german);	
 		return words;
 	} else {
 		const translationEntry = getTranslationEntry(input);
 		const translationWordIds = translationEntry?.manischIds ?? [];
-		const words = manischToGerman
-			.filter((item) => translationWordIds.includes(item.id))
-			.map(item => item.manisch);	
+		const words = translationWordIds
+			.map(twId => {
+				const manischEntry = manischToGerman.find(m2g => m2g.id === twId);
+				if (!manischEntry) throw new Error(`manischEntry ${manischEntry} not found`);
+				return manischEntry.manisch;
+			});
 		return words;
 	}
 }
