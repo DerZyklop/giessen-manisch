748de30ea6078f795077f63bf6284e1293d269fc
Nils <nils@dr-plano.com>
2024-07-28 17:20:46 +0200

    ✨ embedded video

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,8 +10,10 @@
     <section class="mb-10">
       <blockquote>Innerhalb der traditionellen Quartierbevölkerung sowohl der Gummiinsel als auch der Margaretenhütte war mindestens bis in die 1980er Jahre ein stark vom Romanes geprägter, als Manisch bezeichneter Soziolekt verbreitet.[4] Heute dürfte das Manische nur mehr in Relikten vorhanden sein und seinen ursprünglichen Charakter als Geheimsprache weitgehend eingebüßt haben.</blockquote>
       <small>– Text <a href="/translation/manisch/727">getschurt</a> bei <a href="https://de.wikipedia.org/wiki/Gummiinsel">Wikipedia: Gummiinsel</a></small>
-
-      https://www.mediathek-hessen.de/medienview_24960_Marieke-Wist-Das-Manische-in-Giessen.html
+      <br>
+    </section>
+    <section class="mb-10">
+      <iframe src="https://www.mediathek-hessen.de/embed?v=24960" width="100%" height="780" scrolling="no" style="border:0px"></iframe>
     </section>
   </div>
 </template>
